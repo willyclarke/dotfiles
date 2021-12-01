790d4e6b1bd1b194b20cf76d2cd16e267f66ba78
Willy Clarke <willyclarke@mac.com>
2021-12-01 10:28:00 +0100

    BUGFIX: Update lua config work in progress

--- nvim/init/lua/plugins.lua
+++ nvim/init/lua/plugins.lua
@@ -1,40 +1,112 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print('-- bootstrapped packer. Restart nvim to finalize')
+local on_windows = vim.loop.os_uname().version:match 'Windows'
+
+local function join_paths(...)
+  local path_sep = on_windows and '\\' or '/'
+  local result = table.concat({ ... }, path_sep)
+  return result
 end
 
-vim.cmd [[
-  augroup Packer
-    autocmd!
-    autocmd BufWritePost init.lua PackerCompile
-  augroup end
-]]
+vim.cmd [[set runtimepath=$VIMRUNTIME]]
 
-return require('packer').startup(function(use)
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
+local temp_dir
+if on_windows then
+  temp_dir = vim.loop.os_getenv 'TEMP'
+else
+  temp_dir = '/tmp'
+end
 
-  -- git related plugins
-  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+vim.cmd('set packpath=' .. join_paths(temp_dir, 'nvim', 'site'))
 
-  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
+local package_root = join_paths(temp_dir, 'nvim', 'site', 'pack')
+local install_path = join_paths(package_root, 'packer', 'start', 'packer.nvim')
+local compile_path = join_paths(install_path, 'plugin', 'packer_compiled.lua')
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+local function load_plugins()
+  require('packer').startup {
+    {
+      { 'wbthomason/packer.nvim' },
+      { 'neovim/nvim-lspconfig' },
+      { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' },
+      { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    },
+    config = {
+      package_root = package_root,
+      compile_path = compile_path,
+    },
+  }
+end
 
-  use { 'neovim/nvim-lspconfig' , opt = true }
-
-  --use { 'hrsh7th/nvim-cmp'  } -- Autocompletion plugin
-
-  --use { 'hrsh7th/cmp-nvim-lsp' }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
+_G.load_config = function()
+  vim.lsp.set_log_level 'trace'
+  if vim.fn.has 'nvim-0.5.1' == 1 then
+    require('vim.lsp.log').set_format_func(vim.inspect)
   end
-end)
+  local nvim_lsp = require 'lspconfig'
+  local on_attach = function(_, bufnr)
+    local function buf_set_keymap(...)
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+      vim.api.nvim_buf_set_option(bufnr, ...)
+    end
 
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap = true, silent = true }
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    --buf_set_keymap('v', '<leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<CR>', opts)
+    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
+    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  end
+
+  -- Add the server that troubles you here
+  local name = 'clangd'
+  local cmd = { "clangd", "--background-index" }
+
+  if not name then
+    print 'You have not defined a server name, please edit minimal_init.lua'
+  end
+  if not nvim_lsp[name].document_config.default_config.cmd and not cmd then
+    print [[You have not defined a server default cmd for a server
+      that requires it please edit minimal_init.lua]]
+  end
+
+  nvim_lsp[name].setup {
+    cmd = cmd,
+    on_attach = on_attach,
+  }
+
+  print [[You can find your log at $HOME/.cache/nvim/lsp.log. Please paste in a github issue under a details tag as described in the issue template.]]
+end
+
+print('will check the install of packer')
+
+if vim.fn.isdirectory(install_path) == 0 then
+  vim.fn.system { 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path }
+  load_plugins()
+  require('packer').sync()
+  vim.cmd [[autocmd User PackerComplete ++once lua load_config()]]
+else
+  load_plugins()
+  require('packer').sync()
+  _G.load_config()
+  print('Have loaded config')
+end
 
