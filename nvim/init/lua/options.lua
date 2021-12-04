c44b32ca38ddd4d6fd2115af7a1959ca5aac52a9
Willy Clarke <willyclarke@mac.com>
2021-12-04 08:29:52 +0100

    BUGFIX: Set listchars

--- nvim/init/lua/options.lua
+++ nvim/init/lua/options.lua
@@ -10,9 +10,12 @@ vim.o.termguicolors   = true    -- Set Terminal Colors
 vim.o.title           = true    -- Display File Info on Title
 vim.o.showmode        = false   -- Don't Show MODES
 vim.wo.signcolumn     = 'yes'   -- Sign Column
+vim.opt.listchars     = { tab = '→~', eol='¬', trail='⋅', extends='❯',precedes='❮' }-- toggle invisible characters
+vim.opt.list          = true
+
 
 -- Behavior
-vim.o.hlsearch        = false 	-- Set highlight on search
+vim.o.hlsearch        = false -- Set highlight on search
 vim.o.ignorecase      = true 	-- Case insensitive searching
 vim.o.smartcase       = true	-- If Upper Case Char > case sensitive search
 vim.o.smarttab        = true	-- Smart Tabs
@@ -23,9 +26,10 @@ vim.o.expandtab       = true
 vim.o.tabstop         = 2	    -- Tabstop
 vim.o.softtabstop     = 2
 vim.o.shiftwidth      = 2
-vim.o.scrolloff       = 12  	    -- Vertical Scroll Offset
-vim.o.sidescrolloff   = 8  	    -- Horizontal Scroll Offset
-vim.o.mouse           = 'a'	    -- Enable mouse mode
+vim.o.scrolloff       = 12  	-- Vertical Scroll Offset
+vim.o.sidescrolloff   = 8  	  -- Horizontal Scroll Offset
+vim.o.mouse           = 'a'	  -- Enable mouse mode
+vim.o.wrap            = false
 
 -- Vim specific
 vim.o.hidden          = true	    -- Do not save when switching buffers
@@ -34,10 +38,10 @@ vim.o.backup          = false	    -- Disable Backup
 vim.o.swapfile        = false	    -- Don't create Swap Files
 vim.o.spell           = false
 vim.o.undofile        = true 	    -- Save undo history
-vim.o.updatetime      = 250	        -- Decrease update time
-vim.o.timeoutlen      = 250	        -- Time for mapped sequence to complete (in ms)
-vim.o.inccommand      = 'nosplit'   -- Incremental live completion
-vim.o.fileencoding    = "utf-8"	    -- Set File Encoding
+vim.o.updatetime      = 250	      -- Decrease update time
+vim.o.timeoutlen      = 250	      -- Time for mapped sequence to complete (in ms)
+vim.o.inccommand      = 'nosplit' -- Incremental live completion
+vim.o.fileencoding    = "utf-8"	  -- Set File Encoding
 vim.o.spelllang       = "en"
 vim.o.completeopt     = "menuone,noselect"  -- Autocompletion
 vim.opt.shortmess:append { W = true, a = true }
