8854cf4d7caf30f6668fc5863c822aa1f8472225
Willy Clarke <willyclarke@mac.com>
2021-12-05 08:37:37 +0100

    ENHANCEMENT: Add config updates

--- nvim/init/lua/options.lua
+++ nvim/init/lua/options.lua
@@ -6,12 +6,14 @@ vim.o.showtabline     = 2 	    -- Always Show Tabline
 vim.o.pumheight       = 10      -- Pop up Menu Height
 vim.wo.number         = true    -- Display Line Number
 vim.wo.relativenumber = true    -- Make relative line numbers default
+vim.wo.numberwidth    = 5
 vim.o.termguicolors   = true    -- Set Terminal Colors
 vim.o.title           = true    -- Display File Info on Title
-vim.o.showmode        = false   -- Don't Show MODES
+vim.o.showmode        = true    -- Do Show MODES
 vim.wo.signcolumn     = 'yes'   -- Sign Column
 vim.opt.listchars     = { tab = '→~', eol='¬', trail='⋅', extends='❯',precedes='❮' }-- toggle invisible characters
 vim.opt.list          = true
+vim.opt.showbreak     = '↪'
 
 
 -- Behavior
@@ -30,6 +32,7 @@ vim.o.scrolloff       = 12  	-- Vertical Scroll Offset
 vim.o.sidescrolloff   = 8  	  -- Horizontal Scroll Offset
 vim.o.mouse           = 'a'	  -- Enable mouse mode
 vim.o.wrap            = false
+vim.o.autowriteall    = true  -- write all files when hiding buffers. NOTE: causes uncoditional writes
 
 -- Vim specific
 vim.o.hidden          = true	    -- Do not save when switching buffers
@@ -39,13 +42,18 @@ vim.o.swapfile        = false	    -- Don't create Swap Files
 vim.o.spell           = false
 vim.o.undofile        = true 	    -- Save undo history
 vim.o.updatetime      = 250	      -- Decrease update time
-vim.o.timeoutlen      = 250	      -- Time for mapped sequence to complete (in ms)
+vim.o.timeoutlen      = 500	      -- Time for mapped sequence to complete (in ms)
 vim.o.inccommand      = 'nosplit' -- Incremental live completion
 vim.o.fileencoding    = "utf-8"	  -- Set File Encoding
 vim.o.spelllang       = "en"
 vim.o.completeopt     = "menuone,noselect"  -- Autocompletion
 vim.opt.shortmess:append { W = true, a = true }
 vim.o.undodir         = vim.fn.stdpath("cache") .. "/undo"
+vim.o.ruler           = true
+vim.o.exrc            = true
+vim.o.secure          = true
+vim.o.wildmenu        = true      -- Display all matching files when we tab complete
+vim.o.path            = '**'
 -- stylua: ignore end
 
 -- Highlight on yank
@@ -57,6 +65,18 @@ vim.api.nvim_exec(
   augroup end
 ]],
 	false
+)
+
+-- Enable relative number in normal mode and actual line number in insert mode.
+vim.api.nvim_exec(
+  [[
+  augroup toggle_relative_number
+  autocmd!
+  autocmd InsertEnter * :setlocal norelativenumber
+  autocmd InsertLeave * :setlocal relativenumber
+  augroup end
+  ]],
+    false
 )
 
 -- Disable some default plugins
