5a48873956aaf9dea4a43bba28afbc87341a04f6
Willy Clarke <willyclarke@mac.com>
2021-12-05 19:25:11 +0100

    BUGFIX: Move vim files to vim directory

--- nvim/init/init.lua
+++ nvim/init/init.lua
@@ -2,11 +2,11 @@
 -- https://yashguptaz.hashnode.dev/configuring-neovim-05-with-lua-the-basics
 -- which is a tutorial on how to set up neovim with Lua
 --
-require('plugins') 
+require('plugins')
 
 require('options')
 require('keymaps')
-require('settings')   
+require('settings')
 
 -- Configure the various plugins
 require('ww-neogit-config')
@@ -19,6 +19,6 @@ require('ww-gitsigns-config') -- require('ww-vgit-config') -- Alternative to git
 require('ww-kommentary-config')
 
 -- Reuse some of my older files - create a Lua config for this ?
-vim.cmd([[source ~/.config/nvim/abbreviations.vim]])
-vim.cmd([[source ~/.config/nvim/colorscheme.vim]])
-vim.cmd([[source ~/.config/nvim/ww-word-highlight.vim]])
+vim.cmd([[source ~/.config/nvim/vim/abbreviations.vim]])
+vim.cmd([[source ~/.config/nvim/vim/colorscheme.vim]])
+vim.cmd([[source ~/.config/nvim/vim/ww-word-highlight.vim]])
