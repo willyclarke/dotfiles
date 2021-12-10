e1f3e409bcd84b6f9af0b2601c403b25b0adfea2
Willy Clarke <willyclarke@mac.com>
2021-12-10 20:14:40 +0100

    ENHANCEMENT: Add plugin for startify

--- nvim/init/lua/plugins.lua
+++ nvim/init/lua/plugins.lua
@@ -9,29 +9,37 @@ end
 
 return require('packer').startup(function(use)
   -- Language server and autocompletion plugins
-use { 'neovim/nvim-lspconfig' }
-use { 'hrsh7th/cmp-nvim-lsp' }
-use { 'hrsh7th/cmp-nvim-lsp' }
-use { 'hrsh7th/cmp-buffer' }
-use { 'hrsh7th/cmp-path' }
-use { 'hrsh7th/cmp-cmdline' }
-use { 'hrsh7th/nvim-cmp' }
+  use { 'neovim/nvim-lspconfig' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
+  use { 'hrsh7th/cmp-buffer' }
+  use { 'hrsh7th/cmp-path' }
+  use { 'hrsh7th/cmp-cmdline' }
+  use { 'hrsh7th/nvim-cmp' }
 
-use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
-use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
-use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+  use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 
 -- Super fast git decorations implemented purely in lua/teal.
-use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim', tag = 'release' } } -- tag = 'release' -- To use the latest release
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim', tag = 'release' } } -- tag = 'release' -- To use the latest release
 
 -- Simple status line component that shows context of the current cursor position in file
-use { "SmiteshP/nvim-gps", requires = "nvim-treesitter/nvim-treesitter" }
+  use { "SmiteshP/nvim-gps", requires = "nvim-treesitter/nvim-treesitter" }
 
-use {'kyazdani42/nvim-web-devicons' }
-use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
+  use {'kyazdani42/nvim-web-devicons' }
+  use { 'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
 
 -- Neovim plugin to comment in and out text.
-use { 'b3nj5m1n/kommentary' }
+  use { 'b3nj5m1n/kommentary' }
+
+  use {
+    'goolord/alpha-nvim',
+    requires = { 'kyazdani42/nvim-web-devicons' },
+    config = function ()
+      require'alpha'.setup(require'alpha.themes.startify'.opts)
+    end
+  }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
