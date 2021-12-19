6f4170a097fa1785bdbad52cea34327a99fc77e8
Willy Clarke <willyclarke@mac.com>
2021-12-19 07:49:36 +0100

    BUGFIX: Update plugins.lua to more robust bootstrap

--- nvim/init/lua/plugins.lua
+++ nvim/init/lua/plugins.lua
@@ -1,13 +1,52 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- -- -- --
 
 print(install_path)
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
-
 return require('packer').startup(function(use)
+  use "wbthomason/packer.nvim"                -- Have packer manage itself
+  use "nvim-lua/popup.nvim"                   -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim"                 -- Useful lua functions used ny lots of plugins
+
   -- Language server and autocompletion plugins
   use { 'neovim/nvim-lspconfig' }
   use { 'hrsh7th/cmp-nvim-lsp' }
@@ -15,8 +54,6 @@ return require('packer').startup(function(use)
   use { 'hrsh7th/cmp-path' }
   use { 'hrsh7th/cmp-cmdline' }
   use { 'hrsh7th/nvim-cmp' }
-  use { 'nvim-lua/plenary.nvim' }
-
 
   use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
@@ -25,10 +62,10 @@ return require('packer').startup(function(use)
 
   -- Alternative git decorator -  DONT USE BOTH
   -- use({ 'tanvirtin/vgit.nvim', event = 'BufWinEnter', requires = { 'nvim-lua/plenary.nvim' } })
-  use({ 'tanvirtin/vgit.nvim', requires = { 'nvim-lua/plenary.nvim' } })
+  -- use({ 'tanvirtin/vgit.nvim', requires = { 'nvim-lua/plenary.nvim' } })
   -- Alternative git decorator -  DONT USE BOTH
   -- Super fast git decorations implemented purely in lua/teal.
-  -- use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' } -- tag = 'release' -- To use the latest release
+  use { 'lewis6991/gitsigns.nvim', requires = 'nvim-lua/plenary.nvim' } -- tag = 'release' -- To use the latest release
 
   -- Simple status line component that shows context of the current cursor position in file
   use { "SmiteshP/nvim-gps", requires = "nvim-treesitter/nvim-treesitter" }
@@ -49,7 +86,7 @@ return require('packer').startup(function(use)
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
-  if packer_bootstrap then
+  if PACKER_BOOTSTRAP then
     require('packer').sync()
   end
 end)
