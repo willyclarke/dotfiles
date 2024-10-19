7e1c43d1c0e5cbe3d5cd99212c98de9cb8aeba82
Willy Clarke <willy@clarke.no>
2024-10-19 14:46:17 +0000

    BUGFIX: Disable netrw at start of init.lua

--- nvim/kickstart/init.lua
+++ nvim/kickstart/init.lua
@@ -1,3 +1,11 @@
+--
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
 -- Install packer
 local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 local is_bootstrap = false
