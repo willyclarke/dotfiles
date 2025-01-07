078717bd64f408e1d6c506190e6bd0d6e234653b
Willy Clarke <willyclarke@mac.com>
2025-01-07 20:16:59 +0100

    BUGFIX: Alter the call stack in lazy init.lua

--- nvim/lazycfg/init.lua
+++ nvim/lazycfg/init.lua
@@ -1,8 +1,6 @@
 -- Old style config and keymaps to my liking
 require("config.utils")
 require("config.keymaps")
-require("config.options")
-
 --
 -- Set up a user command to load the OpenAI API key
 -- Key hard-coded to be in ~/.local/bin/keychatgpt.asc.gpg
@@ -19,6 +17,13 @@ require("config.chatgptkey")
 --
 require("config.lazy")
 
+-- 
+-- Call options here in case some plugins override my options.
+-- 
+require("config.options")
+
+-- 
 -- Vim script files comes here ...
+-- 
 vim.cmd("source " .. vim.fn.stdpath("config") .. "/vim/ww-word-highlight.vim")
 vim.cmd("source " .. vim.fn.stdpath("config") .. "/vim/abbreviations.vim")
