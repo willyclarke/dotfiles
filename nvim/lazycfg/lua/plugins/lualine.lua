fbae3bf8e1731e5d88265a178d1afa160d0848a7
Willy Clarke <willyclarke@mac.com>
2025-02-13 16:46:30 +0100

    ENHANCEMENT: Add filename to tab

--- nvim/lazycfg/lua/plugins/lualine.lua
+++ nvim/lazycfg/lua/plugins/lualine.lua
@@ -1,34 +1,42 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    enabled = true,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    highlights = true,
+  'nvim-lualine/lualine.nvim',
+  enabled = true,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  highlights = true,
 
-    options = { theme = 'auto' },
+  options = { theme = 'auto' },
 
-    config = function()
-        -- Check if "nvim-navic" can be required
-        local ok, navic = pcall(require, "nvim-navic")
-        if not ok then
-            print('FileType-navic-error: Failed to load nvim-navic')
-            return
-        end
-        require("lualine").setup({
-            sections = {
-                lualine_c = {
-                    {
-                        function()
-                            return navic.get_location()
-                        end,
-                        cond = function()
-                            return navic.is_available()
-                        end,
-                        color = function()
-                            return { fg = vim.bo.modified and '#aa3355' or '#33aa88' }
-                        end,
-                    },
-                }
-            }
-        })
+  config = function()
+    -- Check if "nvim-navic" can be required
+    local ok, navic = pcall(require, "nvim-navic")
+    if not ok then
+      print('FileType-navic-error: Failed to load nvim-navic')
+      return
     end
+    require("lualine").setup({
+      sections = {
+        lualine_c = {
+          {
+            function()
+              return navic.get_location()
+            end,
+            cond = function()
+              return navic.is_available()
+            end,
+            color = function()
+              return { fg = vim.bo.modified and '#aa3355' or '#33aa88' }
+            end,
+          },
+        },
+      },
+      tabline = {
+        lualine_a = { 'buffers' },
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'tabs' }
+      }
+    })
+  end
 }
